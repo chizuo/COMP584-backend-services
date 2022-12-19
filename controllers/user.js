ac36eb34577a5599b906951a385ba274908311cb
Jonathan <jon.chua51@gmail.com>
2022-12-19 02:42:13 -0800

    test

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ async function getAll() {
 }
 
 async function getById(id) {
-    return await User.findById(id);
+    return await User.findOne({where: { id: id}});
 }
 
 async function create(userParam) {
