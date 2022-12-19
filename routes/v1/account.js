31b8e97ff2e05c78783691d7bdf29dbaba918ce8
Jonathan <jon.chua51@gmail.com>
2022-12-18 16:30:24 -0800

    remove console.log

--- routes/v1/account.js
+++ routes/v1/account.js
@@ -8,7 +8,6 @@ const userController = require('../../controllers/user');
 
 // POST end point for account sign-up 
 router.post('/register', async (req,res,next) => {
-	console.log(req.body);
 	userController.create(req.body)
 		.then(user => res.status(200).json(user))
 		.catch(err => next(err));
@@ -18,7 +17,6 @@ router.post('/register', async (req,res,next) => {
 router.get('/login', async (req, res, next) => {
 	const auth = new Buffer.from(req.headers.authorization.split(' ')[1], 'base64').toString().split(':');
 	const login = { username: auth[0], password: auth[1] }
-	console.log(login);
 	userController.authenticate(login)
 		.then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
 		.catch(err => next(err));
